ab2bf7c66fc19ebc55916e1e021efecdb9023d91
Jonas <jonasg@umich.edu>
2025-01-26 00:30:35 -0500

    Add how much a company moved by

--- islands/CompanyRankingRow.tsx
+++ islands/CompanyRankingRow.tsx
@@ -24,22 +24,35 @@ export const CompanyRankingRow = (
     >
       <div class="flex flex-row justify-between font-semibold">
         <div class="flex flex-row">
+          {/* Ranking is 0-indexed - i.e. 0, 1, 2 but we want 1, 2, 3 */}
+          <p class={`mr-4 text-gray-${colorTheme.textValue}`}>{ranking + 1}</p>
           <p class={`text-gray-${colorTheme.textValue} mr-3`}>
             {company.previous_ranking > ranking + 1
-              ? <img width="20" height="20" src="/up-arrow-green.svg" />
+              ? (
+                <div class="flex items-center">
+                  <img width="20" height="20" src="/up-arrow-green.svg" />
+                  <p class="text-xs text-green-600 ml-1">
+                    {company.previous_ranking - (ranking + 1)}
+                  </p>
+                </div>
+              )
               : company.previous_ranking < ranking + 1
               ? (
-                <img
-                  width="20"
-                  height="20"
-                  class="rotate-180"
-                  src="/up-arrow-red.svg"
-                />
+                <div class="flex items-center">
+                  <img
+                    width="20"
+                    height="20"
+                    class="rotate-180"
+                    src="/up-arrow-red.svg"
+                  />
+                  <p class="text-xs text-red-600 ml-1">
+                    {ranking + 1 - company.previous_ranking}
+                  </p>
+                </div>
               )
               : <img width="20" height="20" src="/horizontal-line.svg" />}
           </p>
-          {/* Ranking is 0-indexed - i.e. 0, 1, 2 but we want 1, 2, 3 */}
-          <p class={`mr-3 text-gray-${colorTheme.textValue}`}>{ranking + 1}</p>
+
           <p class={`text-gray-${colorTheme.textValue}`}>{company.name}</p>
         </div>
         <p class={`text-gray-${colorTheme.textValue}`}>
