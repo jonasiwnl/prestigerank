706d7ced853621f05fd27bcd67ec2169fe1e5709
Jonas <jonasg@umich.edu>
2025-03-12 15:37:50 -0400

    Add copyright symbol LMAO

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,13 @@
 export const Footer = () => {
   return (
     <footer class="flex gap-6 w-full mt-28 mb-10 font-semibold">
+      <p>© 2025</p>
       <a class="hover:text-orange-400" href="/about">
         <p>about</p>
       </a>
-      {
-        <a class="hover:text-orange-400" href="/message">
-          <p>leave a message</p>
-        </a>
-      }
+      <a class="hover:text-orange-400" href="/message">
+        <p>leave a message</p>
+      </a>
     </footer>
   );
 };
