66b874d16c5f5ef30e05d15cf431cd6e38854c74
Jonas <jonasg@umich.edu>
2025-04-09 14:38:48 -0400

    Revert "Client side nav"
    This reverts commit 023983b50d512402e3d5e15a673fe4e15c29269d.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,4 @@
 import { type PageProps } from "$fresh/server.ts";
-import { Partial } from "$fresh/runtime.ts";
 import { Footer } from "../components/Footer.tsx";
 import { Header } from "../components/Header.tsx";
 export default function App({ Component }: PageProps) {
@@ -14,12 +13,10 @@ export default function App({ Component }: PageProps) {
           href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
         />
       </head>
-      <body f-client-nav class="bg-background text-foreground">
+      <body class="bg-background text-foreground">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center px-4">
           <Header />
-          <Partial name="page">
-            <Component />
-          </Partial>
+          <Component />
           <Footer />
         </div>
       </body>
