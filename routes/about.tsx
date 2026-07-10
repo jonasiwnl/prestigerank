e2ac01a10f5467ad6571b257def8db057a7c5ffa
Jonas <jonasg@umich.edu>
2026-07-10 01:00:05 -0400

    More about

--- routes/about.tsx
+++ routes/about.tsx
@@ -12,6 +12,18 @@ export default function About() {
         </a>. I was saddened that the rankings were no longer being updated, so
         I decided to make my own.
       </p>
+      <p class="font-semibold mt-8">
+        Anyway, a lot of people complain about these rankings. If you disagree with the algorithm, feel free to <a href="https://github.com/jonasiwnl/prestigerank/pulls" class="hover:text-primary underline" target="_blank">open a PR</a>. It just follows <a href="https://en.wikipedia.org/wiki/Elo_rating_system" class="hover:text-primary underline" target="_blank">this Wikipedia article</a> with a k-factor of between 0 and 2000, so more battles means a company's elo will be less volatile.
+      </p>
+      <p class="font-semibold mt-8">
+        So far, it seems like no one has been able to exploit the rankings (I may be wrong on this, as a friend told me in the past that eBay was at #1.)
+        But I encourage people to try, just because it would be interesting. I'll even tell you how it works. For each generated battle, a token is generated and stored
+        along with the request IP. When a certain company is picked, the IP and token sent along with the company choice must match what's stored on the backend. If it doesn't
+        the request is rejected. Tokens are swept every 24h or so, so I guess you could get really unlucky. There's also rate limiting based on token generation and IP but it's pretty generous iirc.
+      </p>
+      <p class="font-semibold mt-8">
+        People also frequently request for certain companies to be added. This is easy but tedious for me (~1min) as I haven't automated it yet and I'm very lazy, so bear with me; I'll add them eventually.
+      </p>
     </div>
   );
 }
