781401a74d98a5591df219270b954463b2970c01
Jonas <jonasg@umich.edu>
2026-07-10 18:26:07 -0400

    Try

--- components/Ad.tsx
+++ components/Ad.tsx
@@ -4,7 +4,17 @@ export function Ad() {
       class="w-[320px] h-[50px] mx-auto mt-6"
       aria-label="Advertisement"
     >
-      <script src="/adsterra-options.js"></script>
+      <script>
+        {`
+  atOptions = {
+    'key' : '27ef6b090f99c5183fb8998d9f8e132e',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+`}
+      </script>
       <script src="https://www.highperformanceformat.com/27ef6b090f99c5183fb8998d9f8e132e/invoke.js">
       </script>
     </div>
--- static/adsterra-options.js
+++
@@ -1,7 +0,0 @@
-globalThis.atOptions = {
-  "key": "27ef6b090f99c5183fb8998d9f8e132e",
-  "format": "iframe",
-  "height": 50,
-  "width": 320,
-  "params": {},
-};
