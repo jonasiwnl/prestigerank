fd04ee2f1c5e779cee030bc1dfc0b1c38c85d016
Jonas <jonasg@umich.edu>
2026-07-10 17:47:26 -0400

    Idk

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -20,6 +20,16 @@ export default function App({ Component }: PageProps) {
           <Component />
           <Footer />
         </div>
+        <script dangerouslySetInnerHTML={{
+          __html: `window.atOptions = {
+            'key' : '27ef6b090f99c5183fb8998d9f8e132e',
+            'format' : 'iframe',
+            'height' : 50,
+            'width' : 320,
+            'params' : {}
+          };`
+        }}></script>
+        <script src="https://www.highperformanceformat.com/27ef6b090f99c5183fb8998d9f8e132e/invoke.js" defer></script>
       </body>
     </html>
   );
